844fb26d4cdccd3012a93a912a0bd88919915192
nyeholt <nyeholt@gmail.com>
2010-12-06 14:44:40 +0700

    Updated to make list importation easier

--- code/controllers/DataListAdmin.php
+++ code/controllers/DataListAdmin.php
@@ -46,4 +46,27 @@ class DataListCsvLoader extends CsvBulkLoader {
 			),
 		);
 	}
+
+	protected function processRecord($record, $columnMap, &$results, $preview = false) {
+		$class = $this->objectClass;
+		
+		$title = trim($record['Title']);
+		$item = trim($record['ListItem']);
+
+		$existingList = DataList::get_data_list($title);
+		if (!$existingList) {
+			$existingList = new DataList;
+			$existingList->Title = $title;
+			$existingList->write();
+		}
+
+		// now add the item to that list
+		$existingItem = DataObject::get_one('DataListItem', '"Title"=\''.Convert::raw2sql($item).'\' AND "ListID" = '.((int) $existingList->ID));
+		if (!$existingItem) {
+			$existingItem = new DataListItem;
+			$existingItem->Title = $item;
+			$existingItem->ListID = $existingList->ID;
+			$existingItem->write();
+		}
+	}
 }
