53b21c00548f488b80825c99ba3b45ac3c3ef9e6
Marcus Nyeholt <nyeholt@gmail.com>
2012-01-16 11:25:14 +1100

    BUGFIX: Make sure getItemByTitle returns an item from '' list

--- code/dataobjects/DataList.php
+++ code/dataobjects/DataList.php
@@ -1,31 +1,11 @@
 <?php
-/*
-
-Copyright (c) 2009, SilverStripe Australia PTY LTD - www.silverstripe.com.au
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of SilverStripe nor the names of its contributors may be used to endorse or promote products derived from this software
-      without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-OF SUCH DAMAGE.
-*/
-
 /**
  * A data list is a user specified list of data items that can be used
  * for a variety of areas in the site where a predefined list is used
  * using the DataListField form control.
  *
  * @author Marcus Nyeholt <marcus@silverstripe.com.au>
+ * @license BSD License http://silverstripe.org/bsd-license
  */
 class DataList extends DataObject {
     public static $db = array(
@@ -83,7 +63,7 @@ class DataList extends DataObject {
 
 	public function getItemByTitle($title) {
 		$SQL_title = Convert::raw2sql($title);
-		$item = DataObject::get_one('DataListItem', "\"Title\" = '{$SQL_title}'");
+		$item = DataObject::get_one('DataListItem', "\"ListID\" = $this->ID AND \"Title\" = '{$SQL_title}'");
 		if (!$item || !$item->exists()) {
 			// create item
 			$item = new DataListItem();
