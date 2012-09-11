db97aa0a2505ba01432ebcfb0e28d2a3e8d3f2c9
Shea <shea@silverstripe.com.au>
2012-09-11 10:39:50 +1000

    MINOR check that the SortableGridField module is installed

--- _config.php
+++ _config.php
@@ -5,3 +5,7 @@ define('DYNAMICLIST_MODULE', 'dynamiclists');
 if (basename(dirname(__FILE__)) != DYNAMICLIST_MODULE) {
 	throw new Exception(DYNAMICLIST_MODULE . ' module not installed in correct directory');
 }
+
+if (!class_exists('GridFieldSortableRows')) {
+	throw new Exception('The Dynamic Lists module requires the Sortable Grid Field Module module.');
+}
